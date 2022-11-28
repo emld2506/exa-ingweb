9fbfa75bdb3b72192c6d7fd63d3ccb99dd8b280c
emld2506 <18170382@itculiacan.edu.mx>
2022-11-28 10:04:07 -0700

    Creación de vista de actualización
    Se creó la vista UpdateView. Pendiente de definición e implementación

---
+++ exa-ingweb/UpdateView.swift
@@ -0,0 +1,20 @@
+//
+//  UpdateView.swift
+//  exa-ingweb
+//
+//  Created by CCDM24 on 28/11/22.
+//
+
+import SwiftUI
+
+struct UpdateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateView()
+    }
+}
