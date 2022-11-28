91a3663ac6ca8e0072a26a45decd5cb3c628264b
emld2506 <18170382@itculiacan.edu.mx>
2022-11-28 10:00:43 -0700

    Avance de Vista General
    Se añadieron placeholders para las tabs de la tabView. Añadido DataManager

--- exa-ingweb/ContentView.swift
+++ exa-ingweb/ContentView.swift
@@ -1,15 +1,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let cdManager: DataManager
     var body: some View {
         TabView{
+            tabItem(label: "Registro" -> SaveView){
+                
+            }
             
+            tabItem(label: "Datos" -> ListView){
+                
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(cdManager: DataManager())
     }
 }
