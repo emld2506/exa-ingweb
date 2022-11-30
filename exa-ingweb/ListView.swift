88e3bb76b287d4d853cdb214dada0c20c33645c7
emld2506 <18170382@itculiacan.edu.mx>
2022-11-30 09:02:39 -0600

    Reorganización y añadido de navegación
    Se reorganizaron algunos elementos para evitar error por tiempo de compilación excesivamente largo. Además, se agregó un NavigationLink a cada elemento para conectar con UpdateView

--- exa-ingweb/ListView.swift
+++ exa-ingweb/ListView.swift
@@ -8,40 +8,44 @@
 import SwiftUI
 
 struct ListView: View {
-    var cdManager: DataManager
-    var vArray: [Viga]
+    @State var cdManager: DataManager
+    @State var vArray: [Viga]
     
     var body: some View {
         VStack{
-            List{
-                ForEach(vArray, clv_viga: \.self){
-                    viga in
-                    VStack{
-                        Text(viga.clv_obra ?? "")
-                        Text(viga.clv_viga ?? "")
-                        Text(viga.longitud ?? "")
-                        Text(viga.material ?? "")
-                        Text(viga.peso ?? "")
-                        Spacer()
-                        HStack{
-                            
-                        }
-                    }
-                }.onDelete(perform: {
-                    indexSet in
-                    indexSet.forEach({ index in
-                        let vigaDel = vArray[index]
-                        cdManager.borrarViga(viga: vigaDel)
-                        vArray = cdManager.listaVigas()
+            NavigationView{
+
+                List{
+                    ForEach(vArray, clv_viga: \.self){
+                        viga in
+                        VStack{
+                            NavigationLink(destination: UpdateView(viga: viga, cdManager : cdManager)){
+                                Text(viga.clv_obra ?? "")
+                                Text(viga.clv_viga ?? "")
+                                Text(viga.longitud ?? "")
+                                Text(viga.material ?? "")
+                                Text(viga.peso ?? "")
+                            }
+                        }.padding()
+                    }.onDelete(perform: {
+                        indexSet in
+                        indexSet.forEach({ index in
+                            let vigaDel = vArray[index]
+                            cdManager.borrarViga(viga: vigaDel)
+                            vArray = cdManager.listaVigas()
+                        })
                     })
-                })
-            }
-        }.padding().onAppear(perform: listaElementos())
+                }
+            }.padding
+                .onAppear{
+                    cdManager = DataManager()
+                    listaElementos()}
+        }
             
     }
     
-    mutating func listaElementos() -> Void {
-        self.vArray = cdManager.listaVigas()
+    func listaElementos(){
+        vArray = cdManager.listaVigas()
     }
     
 }
@@ -52,6 +56,6 @@ struct ListView: View {
 
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ListView(cdManager: DataManager(), vArray: [Viga].init())
+        ListView(cdManager: DataManager())
     }
 }
